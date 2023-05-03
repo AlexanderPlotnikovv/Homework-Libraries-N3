b601934f51c80a5509c478a14ab04c7f2a9a4804
Alexander Plotnikov <Appplotnikov2019@gmail.com>
2023-05-03 18:53:36 +0300

    New commit

--- Homework Библиотеки N3/Quadrangle.cpp
+++ Homework Библиотеки N3/Quadrangle.cpp
@@ -7,4 +7,5 @@ void Quadrangle::print_info() {
 	std::cout << "Angles: A = " << A1 << ", B = " << B1 << ", C = " << C1 << ", D = " << D1 << std::endl;
 	std::cout << std::endl;
 }
+Quadrangle::Quadrangle(int a1, int b1, int c1, int d1, int A1, int B1, int C1, int D1, std::string Name) : a1(a1), b1(b1), c1(c1), d1(d1), A1(A1), B1(B1), C1(C1), D1(D1), Figure(Name) {};
 Quadrangle::Quadrangle() : Quadrangle(10, 20, 30, 40, 50, 60, 70, 80, "Quadrangle") {}
--- Homework Библиотеки N3/Triangle.cpp
+++ Homework Библиотеки N3/Triangle.cpp
@@ -8,4 +8,5 @@ void Triangle::print_info() {
 	std::cout << "Angles: A = " << A << ", B = " << B << ", C = " << C << std::endl;
 	std::cout << std::endl;
 }
+Triangle::Triangle(int a, int b, int c, int A, int B, int C, std::string Name) : a(a), b(b), c(c), A(A), B(B), C(C), Figure(Name) {};
 Triangle::Triangle() : Triangle(10, 20, 30, 50, 60, 70, "Triangle") {}
